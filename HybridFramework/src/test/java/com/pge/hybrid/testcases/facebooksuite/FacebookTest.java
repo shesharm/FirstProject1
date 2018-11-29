2b6b260d76ee9f9e315bcf5d8ce987a6f15e453c
shesharm <sharmashekhar86@gmail.com>
2018-11-28 21:54:24 -0800

    third commit

---
+++ HybridFramework/src/test/java/com/pge/hybrid/testcases/facebooksuite/FacebookTest.java
@@ -0,0 +1,25 @@
+package com.pge.hybrid.testcases.facebooksuite;
+
+import java.util.Hashtable;
+
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.Status;
+import com.pge.hybrid.base.BaseTest;
+import com.pge.hybrid.util.Constants;
+import com.pge.hybrid.util.DataUtil;
+
+public class FacebookTest extends BaseTest{
+
+	@Test(dataProvider="getData")
+	public void facebookLogin(Hashtable <String,String> data ) throws Exception {
+		test.log(Status.INFO, "Starting "+ testName);
+
+		if(DataUtil.isSkip(testName, xls) ||data.get(Constants.RUNMODE_COL).equals(Constants.RUNMODE_NO)){
+			test.log(Status.SKIP, "Runmode is set to NO");
+			throw new SkipException("Runmode is set to NO");
+		}		
+	    ds.executeKeywords(testName, xls, data);
+	}
+}
