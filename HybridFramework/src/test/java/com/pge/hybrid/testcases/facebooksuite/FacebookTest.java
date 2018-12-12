7f0bf22b61f4c0d33c4117c683c055e21600fa44
shesharm <sharmashekhar86@gmail.com>
2018-12-12 04:16:13 -0800

    Adding test 1

--- HybridFramework/src/test/java/com/pge/hybrid/testcases/facebooksuite/FacebookTest.java
+++ HybridFramework/src/test/java/com/pge/hybrid/testcases/facebooksuite/FacebookTest.java
@@ -16,7 +16,7 @@ public class FacebookTest extends BaseTest{
 	@Test(dataProvider="getData")
 	public void facebookLogin(Hashtable <String,String> data ) throws Exception {
 		test.log(Status.INFO, "Starting "+ testName);
-
+		System.out.println("Test 1");
 		if(DataUtil.isSkip(testName, xls) ||data.get(Constants.RUNMODE_COL).equals(Constants.RUNMODE_NO)){
 			test.log(Status.SKIP, "Runmode is set to NO");
 			throw new SkipException("Runmode is set to NO");
