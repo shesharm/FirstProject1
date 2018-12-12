882dcdb681afa60cc06acde70a922a60d0e5185e
shesharm <sharmashekhar86@gmail.com>
2018-12-12 04:51:00 -0800

    Added env variable from jenkins

--- HybridFramework/src/test/java/com/pge/hybrid/testcases/facebooksuite/FacebookTest.java
+++ HybridFramework/src/test/java/com/pge/hybrid/testcases/facebooksuite/FacebookTest.java
@@ -17,6 +17,7 @@ public class FacebookTest extends BaseTest{
 	public void facebookLogin(Hashtable <String,String> data ) throws Exception {
 		test.log(Status.INFO, "Starting "+ testName);
 		System.out.println("Test 1");
+		System.out.println(System.getProperty("Username"));
 		if(DataUtil.isSkip(testName, xls) ||data.get(Constants.RUNMODE_COL).equals(Constants.RUNMODE_NO)){
 			test.log(Status.SKIP, "Runmode is set to NO");
 			throw new SkipException("Runmode is set to NO");
@@ -27,6 +28,8 @@ public class FacebookTest extends BaseTest{
 	@Test(dataProvider="getData")
 	public void AppScanSite(Hashtable <String,String> data ) throws Exception {
 		test.log(Status.INFO, "Starting "+ testName);
+		System.out.println("Test 1");
+		System.out.println(System.getProperty("Username"));
 		File file = new File("C:\\test\\test.txt");
 		file.createNewFile();
 		if(DataUtil.isSkip(testName, xls) ||data.get(Constants.RUNMODE_COL).equals(Constants.RUNMODE_NO)){
