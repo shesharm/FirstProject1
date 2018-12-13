8db4d7985cbfc8bc1b15c759b2be4483c474a6e4
shesharm <sharmashekhar86@gmail.com>
2018-12-12 20:39:37 -0800

    Add msg

--- HybridFramework/src/test/java/com/pge/hybrid/testcases/facebooksuite/FacebookTest.java
+++ HybridFramework/src/test/java/com/pge/hybrid/testcases/facebooksuite/FacebookTest.java
@@ -30,7 +30,7 @@ public class FacebookTest extends BaseTest{
 		test.log(Status.INFO, "Starting "+ testName);
 		System.out.println("Test 1");
 		String str1 = System.getProperty("USERNAME");
-		System.out.println(str1);
+		System.out.println("The username is " +str1);
 		File file = new File("C:\\test\\test.txt");
 		file.createNewFile();
 		if(DataUtil.isSkip(testName, xls) ||data.get(Constants.RUNMODE_COL).equals(Constants.RUNMODE_NO)){
