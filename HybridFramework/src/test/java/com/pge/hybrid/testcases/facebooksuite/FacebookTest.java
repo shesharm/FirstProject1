b9d42c84fc243227ac5bc631a8299e7fc5662c24
shesharm <sharmashekhar86@gmail.com>
2018-12-12 21:21:38 -0800

    a

--- HybridFramework/src/test/java/com/pge/hybrid/testcases/facebooksuite/FacebookTest.java
+++ HybridFramework/src/test/java/com/pge/hybrid/testcases/facebooksuite/FacebookTest.java
@@ -16,8 +16,7 @@ public class FacebookTest extends BaseTest{
 	@Test(dataProvider="getData")
 	public void facebookLogin(Hashtable <String,String> data ) throws Exception {
 		test.log(Status.INFO, "Starting "+ testName);
-		System.out.println("Test 1");
-		System.out.println(System.getProperty("USERNAME"));
+
 		if(DataUtil.isSkip(testName, xls) ||data.get(Constants.RUNMODE_COL).equals(Constants.RUNMODE_NO)){
 			test.log(Status.SKIP, "Runmode is set to NO");
 			throw new SkipException("Runmode is set to NO");
